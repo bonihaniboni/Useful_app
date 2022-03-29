0e6294052cd481a7151e850d58379db466de8f45
bonihaniboni <bonhwi3825@knu.ac.kr>
2022-03-29 18:28:37 +0900

    floatingactionbutton

--- app/src/main/java/com/example/useful_app/MainActivity.java
+++ app/src/main/java/com/example/useful_app/MainActivity.java
@@ -2,17 +2,23 @@ package com.example.useful_app;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     Fragment1 fragment1;
     Fragment2 fragment2;
     Fragment3 fragment3;
+
+    FloatingActionButton fabMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +57,19 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 return false;
+            }
+        });
+
+        // 플로팅 메뉴 버튼
+        fabMain = findViewById(R.id.fabMain);
+
+        fabMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "버튼 클릭.",Toast.LENGTH_SHORT);
+                toast.show();
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
             }
         });
     }
