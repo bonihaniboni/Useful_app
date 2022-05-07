f36d57c35f5c760c39da94d1fad490b24e6af97c
bonihaniboni <bonhwi3825@knu.ac.kr>
2022-05-07 20:08:59 +0900

    kfsd

---
+++ SingleDiary/app/src/main/java/org/techtown/singlediary/activities/MainActivity.java
@@ -0,0 +1,96 @@
+package org.techtown.singlediary.activities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import org.techtown.singlediary.R;
+import org.techtown.singlediary.activities.CreateNoteActivity;
+import org.techtown.singlediary.adapters.NotesAdapter;
+import org.techtown.singlediary.database.NotesDatabase;
+import org.techtown.singlediary.entities.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    private RecyclerView notesRecyclerView;
+    private List<Note> noteList;
+    private NotesAdapter notesAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
+        imageAddNoteMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               startActivityForResult(
+                       new Intent(getApplicationContext(), CreateNoteActivity.class),
+                       REQUEST_CODE_ADD_NOTE
+               );
+            }
+        });
+
+        notesRecyclerView = findViewById(R.id.notesRecyclerView);
+        notesRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        noteList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(noteList);
+        notesRecyclerView.setAdapter(notesAdapter);
+
+        getNotes();
+    }
+
+    private void getNotes() {
+
+        @SuppressLint("StaticFieldLeak")
+        class GetNoteTask extends AsyncTask<Void, Void, List<Note>> {
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NotesDatabase.getNotesDatabase(getApplicationContext())
+                        .noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes){
+                super.onPostExecute(notes);
+                if (noteList.size() == 0) {
+                    noteList.addAll(notes);
+                    notesAdapter.notifyDataSetChanged();
+                } else {
+                        noteList.add(0, notes.get(0));
+                        notesAdapter.notifyItemChanged(0);
+                }
+                notesRecyclerView.smoothScrollToPosition(0);
+            }
+        }
+
+        new GetNoteTask().execute();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
+            getNotes();
+
+        }
+    }
+}
